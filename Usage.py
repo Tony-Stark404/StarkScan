de399365f723caa1c7e36b7769817f7ce472eca0
Tony-Stark404 <tonystarkxgojo@gmail.com>
2025-11-02 13:02:10 +0530

    Update usage message format in Usage.py

--- Usage.py
+++ Usage.py
@@ -11,10 +11,7 @@ banner = r'''
   '-._.-'   MR STARK
 '''
 
-usage = """Usage: python3 network_scanner.py <target> <start_port> <end_port>
-Positional: <target>=IP/hostname/CIDR    <start_port>,<end_port>=1-65535
-Quick flags: --threads N   --timeout S   --ping   --json   -o OUTPUT
-Example: python3 network_scanner.py 192.168.1.10 1 1024 --threads 200
-"""
+usage = """[ Usage: python3 network_scanner.py <target> <start_port> <end_port> ]"""
+
 
 print(banner + "\n" + usage)
